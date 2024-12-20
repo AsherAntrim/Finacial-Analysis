df2f39c988a375fa45b9ecdd42b60a3b0e0441f4
Asher <aantrim246@cedarville.edu>
2024-12-20 01:07:27 -0500

    Initial Commit

---
+++ Finacial Analysis/Finacial_AnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finacial_AnalysisApp.swift
+//  Finacial Analysis
+//
+//  Created by Asher Antrim on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Finacial_AnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
